017ec00c8ad410a9be8753f44b70bb90e38f3ab2
LinusL612 <Linus.Lundh123@gmail.com>
2026-01-20 10:58:22 +0000

    [Add] getfullyear function

--- webbutveckling VT/mörkmode.js
+++ webbutveckling VT/mörkmode.js
@@ -2,3 +2,7 @@ function LightDarkmode() {
     var element = document.body;
     element.classList.toggle("mörkmode");
 }
+
+let year = new Date().getFullYear();
+
+document.getElementById("Getcurrentyear").innerHTML = year;
