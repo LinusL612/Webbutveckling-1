8145ef3638fe7b06f9386f7cd76fe6b97bf71ffc
LinusL612 <Linus.Lundh123@gmail.com>
2026-01-23 09:21:10 +0000

    [add] localstorage save on mörkmode button

--- webbutveckling VT/mörkmode.js
+++ webbutveckling VT/mörkmode.js
@@ -1,8 +1,30 @@
+function loadTheme(){
+    
+    const thema=localStorage.getItem("theme");
+    
+    if(thema==="mörk") {
+        document.body.classList.add("mörkmode");
+    }
+}
+
+loadTheme();
+
 function LightDarkmode() {
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("mörkmode");
+
+    
+    if(document.body.classList.contains("mörkmode")) {
+        localStorage.setItem("theme","mörk");
+    }
+    
+    else {
+        localStorage.setItem("theme","ljus");
+    }
 }
 
 let year = new Date().getFullYear();
 
 document.getElementById("Getcurrentyear").innerHTML = year;
+
+
