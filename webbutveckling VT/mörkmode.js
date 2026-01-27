d439198b0bdd288b61f4a4ebeb578c9ba8b4745c
LinusL612 <Linus.Lundh123@gmail.com>
2026-01-27 11:01:54 +0000

    [added] flera sidor som har connectad css och kan ha connected javascript men han inte :)

--- webbutveckling VT/mörkmode.js
+++ webbutveckling VT/mörkmode.js
@@ -27,4 +27,21 @@ let year = new Date().getFullYear();
 
 document.getElementById("Getcurrentyear").innerHTML = year;
 
+localStorage.setItem("lastVisit", Date.now())
+
+const FIVE_MINUTES = 1000 * 60 * 5;
+const LastVisit = localStorage.getItem("lastVisit")
+if (LastVisit) {
+    const now = Date.now();
+
+    const timePassed = now - LastVisit;
+
+    if (timePassed > FIVE_MINUTES) {
+        localStorage.removeItem("theme")
+        localStorage.removeItem("lastVisit")
+    }
+}
+
+
+
 
